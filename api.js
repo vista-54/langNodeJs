6b45e97524025d00a3e902c5a4c5b5f21655897f
Vitalii Stalynskyi <vista-54@ukr.net>
2019-09-26 22:35:06 +0300

    Chnaged confi

--- api.js
+++ api.js
@@ -2,30 +2,11 @@ let express = require('express')
 let path = require('path')
 let index = require('./index')
 let app = express()
-const http = require('http');
 // app.use(express.logger('dev'));
 // parse application/json
 // app.use(app.router);
 
-process
-  .on('SIGTERM', shutdown('SIGTERM'))
-  .on('SIGINT', shutdown('SIGINT'))
-  .on('uncaughtException', shutdown('uncaughtException'));
 
-setInterval(console.log.bind(console, 'tick'), 1000);
-http.createServer((req, res) => res.end('hi'))
-  .listen(process.env.PORT || 4001, () => console.log('Listening'));
-
-function shutdown(signal) {
-  return (err) => {
-    console.log(`${ signal }...`);
-    if (err) console.error(err.stack || err);
-    setTimeout(() => {
-      console.log('...waited 5s, exiting.');
-      process.exit(err ? 1 : 0);
-    }, 4001).unref();
-  };
-}
 
 app.get('/api', function (req, res) {
   let body = req.query
